29418bfa40fc439d5ecde956488b891da00609bb
Ansh Dhanani <dhananiansh01@gmail.com>
2025-12-19 09:58:37 +0530

    typo fixed

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@/generated/client/client";
+import { PrismaClient } from "../generated/client/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
